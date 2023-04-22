7596ab4f3965a828dec79e4fc57dbc10bdca3d22
davidsilva02 <davidsilva200210.ds@gmail.com>
2023-04-22 11:33:15 +0100

    remove sptest

--- sptest/sptest/src/main/java/com/sptest/sptest/ServerRMI.java
+++
@@ -1,8 +0,0 @@
-package com.sptest.sptest;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface ServerRMI extends Remote {
-    public String h() throws RemoteException;
-}
